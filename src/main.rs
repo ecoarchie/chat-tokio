42d81319de55679b890492f7649b20a2536f4f0b
ecoarchie <eco.artur@gmail.com>
2023-08-10 17:26:58 +0300

    init commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8090").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel(10);
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (message, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(message.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
